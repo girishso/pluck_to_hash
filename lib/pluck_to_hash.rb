dde890fb6075ec5d2310cf3d9a750b6c374a3d7d
Girish S <girish.sonawane@gmail.com>
2016-05-16 14:05:40 +0530

    added pluck_to_struct method

--- lib/pluck_to_hash.rb
+++ lib/pluck_to_hash.rb
@@ -7,14 +7,7 @@ module PluckToHash
     def pluck_to_hash(*keys)
       block_given = block_given?
       keys = column_names if keys.blank?
-      formatted_keys = keys.map do |k|
-        case k
-        when String
-          k.split(/ as /i)[-1].to_sym
-        when Symbol
-          k
-        end
-      end
+      formatted_keys = format_keys(keys)
       pluck(*keys).map do |row|
         row = [row] if keys.size == 1
         value = HashWithIndifferentAccess[formatted_keys.zip(row)]
@@ -23,7 +16,33 @@ module PluckToHash
       end
     end
 
+    def pluck_to_struct(*keys)
+      block_given = block_given?
+      keys = column_names if keys.blank?
+      formatted_keys = format_keys(keys)
+
+      struct = Struct.new(*formatted_keys)
+      pluck(*keys).map do |row|
+        row = [row] if keys.size == 1
+        value = struct.new(*row)
+        yield(value) if block_given
+        value
+      end
+    end
+
+    def format_keys(keys)
+      keys.map do |k|
+        case k
+        when String
+          k.split(/ as /i)[-1].to_sym
+        when Symbol
+          k
+        end
+      end
+    end
+
     alias_method :pluck_h, :pluck_to_hash
+    alias_method :pluck_s, :pluck_to_struct
   end
 end
 
---
+++ spec/pluck_to_struct_spec.rb
@@ -0,0 +1,80 @@
+require_relative './spec_helper'
+
+describe 'PluckToStruct' do
+  before { TestModel.delete_all }
+
+  context '.pluck_to_struct' do
+    before do
+      TestModel.create!(id: 1, test_attribute: "test1")
+      TestModel.create!(id: 2, test_attribute: "test2")
+      TestModel.create!(id: 3, test_attribute: "test3")
+    end
+
+    it 'plucks the ids, test_attributes of the objects to a struct correctly' do
+      TestModel.all.pluck_to_struct(:test_attribute, :id).each_with_index do |model, ix|
+        id = ix+1
+        expect(model).to be_a(Struct)
+        expect(model.test_attribute).to eq("test#{id}")
+        expect(model.id).to eq(id)
+      end
+    end
+
+    context 'alias pluck_s' do
+      it 'works correctly' do
+        struct = TestModel.where(id: 1).pluck_s(:test_attribute).first
+        expect(struct.test_attribute).to eq("test1")
+      end
+    end
+
+    context 'the model does not have the attribute specified' do
+      it 'raises an error' do
+        expect do
+          TestModel.all.pluck_h(:foo)
+        end.to raise_error
+      end
+    end
+
+    context 'no models exist for the given criteria' do
+      it 'returns an empty relation' do
+        expect do
+          result = TestModel.where(id: -1).pluck_h(:id)
+          expect(result).to be_empty
+        end.to_not raise_error
+      end
+    end
+
+  end
+
+
+  context 'when serialize attributes used' do
+    describe '.pluck_to_struct' do
+      before do
+        TestModel.create!(id: 1, serialized_attribute: [])
+        TestModel.create!(id: 2, serialized_attribute: ['Zygohistomorpic', 'Prepromorphism'])
+        TestModel.create!(id: 3, serialized_attribute: ['Comonad'])
+      end
+      
+      it 'plucks to struct correctly' do
+        result = TestModel.pluck_to_struct(:serialized_attribute)
+
+        expect(result[0].serialized_attribute).to eq([])
+        expect(result[1].serialized_attribute).to eq(['Zygohistomorpic', 'Prepromorphism'])
+        expect(result[2].serialized_attribute).to eq(['Comonad'])
+      end
+
+      it 'plucks to struct with multiple attributes' do
+        result = TestModel.pluck_to_struct(:id, :serialized_attribute)
+
+        expect(result[0].serialized_attribute).to eq([])
+        expect(result[0].id).to eq(1)
+
+        expect(result[1].serialized_attribute).to eq(['Zygohistomorpic', 'Prepromorphism'])
+        expect(result[1].id).to eq(2)
+
+        expect(result[2].serialized_attribute).to eq(['Comonad'])
+        expect(result[2].id).to eq(3)
+      end
+    end
+  end
+
+end
